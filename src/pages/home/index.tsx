e3adfda814a2ceede98448697023e60a415e7c15
shiqi@indexed.cn <shiqi@indexed.cn>
2022-05-07 13:50:11 +0800

    feat: harbor test

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,6 +5,7 @@ const Home = () => {
         <div>
             <h1 className="text-3xl text-green-600">Home</h1>
             <Button type="primary">默认</Button>
+            <p>测试</p>
         </div>
     )
 }
