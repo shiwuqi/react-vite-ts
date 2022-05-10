36d35a0fb007a7ecce0f92bff7eb31f44efb25e3
shiqi@indexed.cn <shiqi@indexed.cn>
2022-05-10 18:19:43 +0800

    feat: ceshi

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,7 @@
-import { Button } from 'antd';
-
 const Home = () => {
     return (
         <div>
-            <h1 className="text-3xl text-green-600">Home</h1>
-            <Button type="primary">默认</Button>
-            <p>测试</p>
+            
         </div>
     )
 }
